914b423b2d3b1ae5f1330ea400083e0871d9cb8c
Adam Streck <adam.streck@gmail.com>
2016-05-30 16:38:05 +0200

    Removed the import duplicate.

--- LibBoolEE.cpp
+++ LibBoolEE.cpp
@@ -14,8 +14,6 @@ You should have received a copy of the GNU General Public License along with
 this program.  If not, see <http://www.gnu.org/licenses/>.
 ******************************************************************************/
 
-
-#include <stdexcept>
 #include "LibBoolEE.h"
 
 std::vector<std::string> LibBoolEE::singleParse(const std::string & formula, const char op) {
